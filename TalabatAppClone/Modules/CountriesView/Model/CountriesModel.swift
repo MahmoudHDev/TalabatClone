fadbc9dca6a74997020caf439cfe7ad98b5c6eec
Mahmoud Hashim <Mahmoud.ios.dev@gmail.com>
2022-08-13 16:09:23 +0200

    adding the second view

---
+++ TalabatAppClone/Modules/CountriesView/Model/CountriesModel.swift
@@ -0,0 +1,8 @@
+//
+//  CountriesModel.swift
+//  TalabatAppClone
+//
+//  Created by Mahmoud on 8/13/22.
+//
+
+import Foundation
---
+++ TalabatAppClone/Modules/CountriesView/View/CountriesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CountriesTableViewCell.swift
+//  TalabatAppClone
+//
+//  Created by Mahmoud on 8/13/22.
+//
+
+import UIKit
+
+class CountriesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
