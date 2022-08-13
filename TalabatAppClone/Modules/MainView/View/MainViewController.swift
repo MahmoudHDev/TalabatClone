d89727155dde306498ca49216c04e8f12facef97
Mahmoud Hashim <Mahmoud.ios.dev@gmail.com>
2022-08-13 13:16:38 +0200

    creating the main view

--- TalabatAppClone/Modules/MainView/View/MainViewController.swift
+++ TalabatAppClone/Modules/MainView/View/MainViewController.swift
@@ -6,22 +6,63 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class MainViewController: UIViewController {
     //MARK:- Outlets
-
-    
+    @IBOutlet weak var topView          : UIView!
+    @IBOutlet weak var bottomView       : UIView!
+    @IBOutlet weak var locationBtn      : UIButton!
+    @IBOutlet weak var manualLocationBtn: UIButton!
     
     //MARK:- Properties
-    //
-    
+    let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "Lunch", ofType: "mp4")!))
+    var platerLooper: AVPlayerLooper!
+    var queuePlayer : AVQueuePlayer!
     
     //MARK:- View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.updateUI()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
+        self.basicPlayer()
+        
+    }
+    
+    //MARK:- Methods
+    private func updateUI() {
+        
+        bottomView.layer.cornerRadius   = 20
+        bottomView.layer.maskedCorners  = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+
+        locationBtn.layer.cornerRadius  = 20
+
+    }
+    
+    private func basicPlayer() {
+
+        player.isMuted      = true
+        let layer           = AVPlayerLayer(player: player)
+        layer.frame         = topView.bounds
+        layer.videoGravity  = .resizeAspectFill
+        topView.layer.addSublayer(layer)
+        player.play()
+    }
+    
+    //MARK:- Actions
+
+    @IBAction private func autoLocationBtn(_ sender: UIButton) {
+     print("CLocationManager")
+    }
+    
+    @IBAction private func manualLocationBtn(_ sender: UIButton) {
+        print("Show Maps")
+    }
 
 }
 
