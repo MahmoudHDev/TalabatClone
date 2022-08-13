5fc43620c272f82de90a59a5711569df21a8c38b
Mahmoud Hashim <Mahmoud.ios.dev@gmail.com>
2022-08-13 10:46:51 +0200

    Initial Commit

---
+++ TalabatAppClone/Modules/MainView/View/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TalabatAppClone
+//
+//  Created by Mahmoud on 8/12/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    //MARK:- Outlets
+
+    
+    
+    //MARK:- Properties
+    
+    
+    
+    //MARK:- View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
