01bd800e2b9c3ec7b5d8c5123289682fd0e4498e
Mahmoud Hashim <Mahmoud.ios.dev@gmail.com>
2022-08-24 08:55:19 +0200

    adding offer collectionView

---
+++ TalabatAppClone/Modules/HomeView/View/CollectionView/Cells/DiscountsCVCell.swift
@@ -0,0 +1,17 @@
+//
+//  DiscountsCVCell.swift
+//  TalabatAppClone
+//
+//  Created by Mahmoud on 8/20/22.
+//
+
+import UIKit
+
+class DiscountsCVCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
